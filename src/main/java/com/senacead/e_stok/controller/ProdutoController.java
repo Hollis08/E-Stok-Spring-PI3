dcc919d6ef49f0d0e99e989d353d26b263261a19
Hollis08 <hollis08tillo@hotmail.com>
2025-05-26 01:00:05 -0300

    Criado classe ProdutoController

--- src/main/java/com/senacead/e_stok/controller/ProdutoController.java
+++ src/main/java/com/senacead/e_stok/controller/ProdutoController.java
@@ -1,9 +1,50 @@
 package com.senacead.e_stok.controller;
 
+import com.senacead.e_stok.model.Produto;
+import com.senacead.e_stok.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProdutoController {
+    @Autowired
+    private ProdutoService produtoService;
+    
+    @GetMapping("/produto-cadastro")
+    public String exibirFormulario(Model model){
+        model.addAttribute("produto", new Produto());
+        return "produto-cadastro";
+    }
+    
+    @PostMapping("produto-gravar")
+    public String processarFormulario(@ModelAttribute Produto produto){
+        produtoService.salvar(produto);
+        return "redirect:/produto-lista";
+    }
+    
+    @GetMapping("/produto-lista")
+    public String exibirListagem(Model model){
+        model.addAttribute("produtos", produtoService.listarProduto());
+        return "produto-lista";
+    }
+    
+    @GetMapping("/produto-alterar/{id}")
+    public String alterar(@PathVariable int id,Model model){
+        model.addAttribute("produto", produtoService.buscarPorId(id));
+        return "produto-cadastro";
+    }
+    
+    @GetMapping("/produto-excluir/{id}")
+    public String excluir(@PathVariable int id){
+        produtoService.excluir(id);
+        return "redirect:/produto-lista";
+    }
+    
     
     
 }
