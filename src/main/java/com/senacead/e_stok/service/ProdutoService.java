70a786414c6ada3a703e0b0b2003b49dc00a6edd
Hollis08 <hollis08tillo@hotmail.com>
2025-05-26 00:47:41 -0300

    Correção dos arquivo ProdutoService

--- src/main/java/com/senacead/e_stok/service/ProdutoService.java
+++ src/main/java/com/senacead/e_stok/service/ProdutoService.java
@@ -1,6 +1,31 @@
 package com.senacead.e_stok.service;
 
+import com.senacead.e_stok.model.Produto;
+import com.senacead.e_stok.repository.ProdutoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class ProdutoService {
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    
+    public Produto salvar(Produto produto){
+        return produtoRepository.save(produto);
+    }
+    
+    public List<Produto> listarProduto(){
+        return produtoRepository.findAll();
+    }
+    
+    public Produto buscarPorId(int id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+    
+    public void excluir(int id){
+        produtoRepository.deleteById(id);
+    }
     
 }
